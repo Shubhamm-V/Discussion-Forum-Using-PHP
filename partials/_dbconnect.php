9387151f195a057231b850f22123d89f2b6e7e3b
Shubhamm-V <shubhamvyavhare30@gmail.com>
2022-02-02 20:27:39 +0530

    Initial commit

---
+++ partials/_dbconnect.php
@@ -0,0 +1,8 @@
+<?php
+// Script to connect to the database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "discuss";
+$conn = mysqli_connect($servername, $username, $password, $database);
+?>
---
+++ partials/_footer.php
@@ -0,0 +1,7 @@
+<br><br><br>
+<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+  <div class="container-fluid">
+   <h9 style="color:white;">&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  Copyright  &copy; Learning Hub</h9>
+  </div>
+</nav>;
+
